57c6745dce51e5fe828127c3426f7de863d01607
Jyunaut <edmartinh@gmail.com>
2021-05-01 17:20:01 -0700

    added player movement

---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerInput
+    {
+        public float Horizontal => Input.GetAxisRaw("Horizontal");
+        public float Vertical   => Input.GetAxisRaw("Vertical");
+        public bool Dodge       => Input.GetButtonDown("Dodge");
+    }
+}
